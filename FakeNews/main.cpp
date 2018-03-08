270dc6385d48e3fc89b75652dca5c53308efc040
Napk1ns <33349429+Napk1ns@users.noreply.github.com>
2018-03-08 18:18:30 +0000

    Update main.cpp
    Moved list class to separate header file, realised white/blacklist outputs were the wrong way around so i just switched them.

--- FakeNews/main.cpp
+++ FakeNews/main.cpp
@@ -5,6 +5,7 @@
 #include <exception>
 
 #include "exc.h"
+#include "lists.h"
 
 // CMP2089M-1718 - Group Project
 // Fake News Detector
@@ -62,78 +63,6 @@ private:
 	string URL;
 };
 
-class Lists
-{
-public:
-	
-	Lists()
-	{}
-	~Lists()
-	{}
-	//simply set whitelist
-	void SetWhitelist(vector<string> str)
-	{
-		whitelist = str;
-	}
-	//push something onto end of whitelist
-	void AddStringWhitelist(string str)
-	{
-		whitelist.push_back(str);
-	}
-	//returns contents of whitelist
-	std::vector<string> GetCopyOfWhitelist()
-	{
-		return whitelist;
-	}
-	//slightly more detailed contents, with line nums
-	void WhitelistVectorContents()
-	{
-		for (unsigned int i = 0; i < whitelist.size(); i++)
-		{
-			std::cout << "Element[" << i << "] = " << whitelist[i] << std::endl;
-		}
-		std::cout << std::endl;
-	}
-	//simply set blacklist
-	void SetBlacklist(vector<string> str)
-	{
-		blacklist = str;
-	}
-	//append something onto end
-	void AddStringBlacklist(string str)
-	{
-		blacklist.push_back(str);
-	}
-	//returns contents of blacklist
-	std::vector<string> GetCopyOfBlacklist()
-	{
-		return blacklist;
-	}
-	//as above
-	void BlacklistVectorContents()
-	{
-		for (unsigned int i = 0; i < blacklist.size(); i++)
-		{
-			std::cout << "Element[" << i << "] = " << blacklist[i] << std::endl;
-		}
-		std::cout << std::endl;
-	}
-	void printWhite()
-	{
-		cout << "WHITELIST:" << endl;
-		for (const string& s : whitelist) cout << s << endl;
-	}
-	void printBlack()
-	{
-		cout << "\nBLACKLIST:" << endl;
-		for (const string& s : blacklist) cout << s << endl;
-	}
-
-private:
-	vector<string> whitelist;
-	vector<string> blacklist;
-
-};
 
 
 int main(int argc, char* argv[])
@@ -158,8 +87,8 @@ int main(int argc, char* argv[])
 
     try
     {
-        lists.SetBlacklist(load_list("whitelist.txt"));
-		lists.SetWhitelist(load_list("blacklist.txt"));
+        lists.SetWhitelist(load_list("whitelist.txt"));
+	lists.SetBlacklist(load_list("blacklist.txt"));
     }
 
     catch (const exc::exception& e)
