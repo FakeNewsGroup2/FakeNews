9d5f96b00ec84f5d0f1c55eaca7d8ff1c3c8ef89
Ethan Ansell <mail@zemja.org>
2018-03-16 22:39:00 +0000

    Compiles with Linux/GCC.

--- FakeNews/fs.cpp
+++ FakeNews/fs.cpp
@@ -14,6 +14,23 @@ using std::string;
 namespace fakenews
 {
 
+// All this does is wraps strerror, so that it compiles with both MSVC and GCC.
+// Behaves like GNU strerror_r.
+char* error(char* buf, size_t len);
+
+#ifdef _MSC_VER
+char* error(char* buf, size_t len)
+{
+    strerror_s(buf, len, errno);
+    return buf;
+}
+#else
+char* error(char* buf, size_t len)
+{
+    return strerror_r(errno, buf, len);
+}
+#endif
+
 namespace fs
 {
 
@@ -25,10 +42,9 @@ vector<string> load_lines(const string& path, bool UNIX)
     if (!input)
     {
         std::stringstream ss;
-        char error[256];
-        // TODO Change this line and it will compile on other operating systems.
-        strerror_s(error, sizeof(error), errno);
-        ss << "File at '" << path << "' could not be opened for reading: " << error;
+        char err[256];
+        ss << "File at '" << path << "' could not be opened for reading: "
+            << error(err, sizeof(err));
         throw exc::file(ss.str());
     }
 
